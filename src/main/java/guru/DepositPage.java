e641a22c98e00b00dc038c4f8f256e79478b51cd
Tuan Nguyen <tuan.nguyen.bk08@gmail.com>
2018-10-03 19:24:08 +0700

    add deposit page

---
+++ src/main/java/guru/DepositPage.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alan
+ * Date: 24/04/13
+ * Time: 11:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+package guru;
+
+public class DepositPage {
+}
